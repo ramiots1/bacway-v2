a500a959f7211af2a051a2f6ed5c65fcd96476ec
Ramiverse <144566504+RamiOtsmane@users.noreply.github.com>
2024-10-25 17:13:54 +0100

    ch

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function darkMode() {
     var element = document.body
     element.classList.toggle("dark-mode");
 }
+
+function toggleMenu() {
+    const menu = document.getElementById('mobileMenu');
+    menu.classList.toggle('menu-open');
+}
